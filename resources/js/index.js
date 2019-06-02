a4a6e495002b2f230413c295d01ae2f3ab9f4d43
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2019-06-02 07:32:27 +0600

    updated files

--- resources/js/index.js
+++ resources/js/index.js
@@ -8,14 +8,18 @@ function show_slide(n) {
     }
               
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '');
+        dots[i].className = dots[i].className.replace(' active-dots', '');
     }
     
     slides[slide_id - 1].style.display = 'block';  
-    dots[slide_id - 1].className += ' active';
+    dots[slide_id - 1].className += ' active-dots';
 }
 
 function current_slide(n) {
     show_slide(slide_id = n);
 }
-            
+
+function toggle_nav(){
+    var main_nav = document.getElementById('js-menu');;
+    main_nav.classList.toggle('active');
+}
